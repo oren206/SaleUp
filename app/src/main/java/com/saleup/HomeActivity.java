b8356f8da45c4ad69828c238892644e008557b82
OrenR <oren.r@cellarix.com>
2016-04-29 14:31:16 +0300

    test sending push notifications

--- app/src/main/java/com/saleup/HomeActivity.java
+++ app/src/main/java/com/saleup/HomeActivity.java
@@ -1,16 +1,33 @@
 package com.saleup;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.iid.InstanceID;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -19,11 +36,20 @@ import butterknife.InjectView;
 public class HomeActivity extends ActionBarActivity {
 
     @InjectView(R.id.lblUserName) TextView _userNameLabel;
+    @InjectView(R.id.btnNotify) Button _notifyButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.inject(this);
+
+        _notifyButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                sendPush();
+            }
+        });
 
         String user = (String) Cache.GetInstance().Get(HomeActivity.this, "UserData");
         try {
@@ -32,12 +58,93 @@ public class HomeActivity extends ActionBarActivity {
 
             _userNameLabel.setText(finalResult.getString("UserName"));
 
-
         }
         catch (JSONException ex){
 
         }
 
+    }
+
+    public void sendPush(){
+
+        final ProgressDialog progressDialog = new ProgressDialog(HomeActivity.this);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Sending...");
+        progressDialog.show();
+
+        new Thread(new MyThread(
+                new OnRunMe(){public Result run(){
+                    try  {
+
+                        String token = "";
+                        String user = (String) Cache.GetInstance().Get(HomeActivity.this, "UserData");
+                        try {
+                            JSONTokener tokener = new JSONTokener(user);
+                            JSONObject finalResult = new JSONObject(tokener);
+
+                            token = finalResult.getString("DeviceId");
+
+
+                        }
+                        catch (JSONException ex){
+
+                        }
+
+                        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+                        HttpPost post = new HttpPost("http://saleup.azurewebsites.net/api/User/TestNotifications");
+
+                        post.addHeader("Content-type", "application/x-www-form-urlencoded");
+                        List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+                        urlParameters.add(new BasicNameValuePair("DeviceId", token));
+
+                        post.setEntity(new UrlEncodedFormEntity(urlParameters));
+
+
+                        HttpResponse responseGet = httpClient.execute(post);
+
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(responseGet.getEntity().getContent(), "UTF-8"));
+                        String json = reader.readLine();
+
+                        JSONTokener tokener = new JSONTokener(json);
+                        JSONObject finalResult = new JSONObject(tokener);
+
+                        httpClient.close();
+
+                        Result result = new Result();
+                        result.status = true;
+                        result.data = finalResult;
+                        return  result;
+
+                    } catch (IOException e) {
+                        Result result = new Result();
+                        result.status = false;
+                        return  result;
+                    }
+                    catch (JSONException e) {
+                        Result result = new Result();
+                        result.status = false;
+                        return  result;
+                    }
+                }},
+                new OnCallback(){public void callback(final Result result){
+                    HomeActivity.this.runOnUiThread(new Runnable() {
+                        public void run() {
+                            Object data = result.data;
+                            progressDialog.dismiss();
+                        }
+                    });
+
+                }},
+                new OnCallback(){public void callback(Result result){
+                    HomeActivity.this.runOnUiThread(new Runnable() {
+                        public void run() {
+
+                            progressDialog.dismiss();
+                        }
+                    });
+
+                }}
+        )).start();
     }
 
     @Override
